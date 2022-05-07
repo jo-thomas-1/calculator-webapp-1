928a401ea8917e7a9d5d5627878c6138fc5340bc
nandana02 <103559843+nandana02@users.noreply.github.com>
2022-05-07 15:07:42 +0530

    removed redundant clear screen code

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 
 let value_1 = 0, value_2 = 0, operator = "";
 
+// clear screen
+function clear_screen()
+{
+	document.getElementById("screen").innerHTML = "";
+}
+
 // input value function for 0 to 9 and '.'
 function value_input(key)
 {
@@ -12,7 +18,7 @@ function value_input(key)
 function operator_input(key)
 {
 	value_1 = parseInt(document.getElementById("screen").innerHTML);
-	document.getElementById("screen").innerHTML = "";
+	clear_screen();
 	operator = key;
 }
 
@@ -20,17 +26,11 @@ function operator_input(key)
 function equals()
 {
 	value_2 = parseInt(document.getElementById("screen").innerHTML);
-	document.getElementById("screen").innerHTML = "";
+	clear_screen();
 
 	// calculation ------------------------------------------------------
 	let result = value_1 + value_2;
 
 	// result output
 	document.getElementById("screen").innerHTML = result;
-}
-
-// clear screen
-function clear_screen()
-{
-	document.getElementById("screen").innerHTML = "";
 }
