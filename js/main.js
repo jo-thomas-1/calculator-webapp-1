faf736af212e56d0c502ef4069918eeda4245dba
silpa1199 <103562388+silpa1199@users.noreply.github.com>
2022-05-07 14:00:45 +0530

    rectified input function to accept multi-digit values

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /* main javascript file */
 
+
+// input value function for 0 to 9 and '.'
 function value_input(key)
 {
-	document.getElementById("screen").innerHTML = key;
+	document.getElementById("screen").innerHTML = document.getElementById("screen").innerHTML + key;
 }
