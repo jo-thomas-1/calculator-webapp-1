8611fe02b36c21afd399bc653d07d5eae886c6dd
nandana02 <103559843+nandana02@users.noreply.github.com>
2022-05-07 14:31:44 +0530

    added functionality for operator buttons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 /* main javascript file */
 
+let value_1 = 0, value_2 = 0, operator = "";
 
 // input value function for 0 to 9 and '.'
 function value_input(key)
 {
 	document.getElementById("screen").innerHTML = document.getElementById("screen").innerHTML + key;
+}
+
+// function for operator buttons
+function operator_input(key)
+{
+	value_1 = parseInt(document.getElementById("screen").innerHTML);
+	document.getElementById("screen").innerHTML = "";
+	operator = key;
 }
